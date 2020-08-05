c7827a89901d287534d612993ae16c9db2976149
Shailendra Singh <shailendra9920302728@gmail.com>
2020-08-05 21:59:51 +0530

    Initial Commit

---
+++ src/com/myprojects/employeedetails/client/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package com.myprojects.employeedetails.client;
+
+import com.myprojects.employeedetails.shared.FieldVerifier;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class EmployeeDetails implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/com/myprojects/employeedetails/client/Service.java
@@ -0,0 +1,8 @@
+package com.myprojects.employeedetails.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface Service extends RemoteService {
+	
+
+}
